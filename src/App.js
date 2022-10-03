1f5f07dd47d58f6d7b22c679c29d5b8a64168963
fesmjke <fatalerror.sm@gmail.com>
2022-10-03 22:58:01 +0300

    added fractals to App component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import HFractal from "./components/fractals/h-fractal";
+import Koch from "./components/fractals/koch";
 
 function App() {
   return (
     <div className="App">
-      
+      <Koch></Koch>
+      <HFractal></HFractal>
     </div>
   );
 }
