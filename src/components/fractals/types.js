b3caee18b6245f475c088892a6b434a74a58e9ef
fesmjke <fatalerror.sm@gmail.com>
2022-10-18 01:04:38 +0300

    changed language for view, and added lines types

--- src/components/fractals/types.js
+++ src/components/fractals/types.js
@@ -1,5 +1,24 @@
 export const FRACTAL_TYPES = {
-    KOCH : 'Koch Snowflake',
-    INVERSED : 'Inversed Koch Snowflake',
-    H : 'H-Fractal'
+    KOCH : 'Сніжинка Коха',
+    INVERSED : 'Сніжинка Коха навпаки',
+    H : 'Н-Фрактал'
+}
+
+export const LINES_TYPES = {
+    STRAIGHT : {
+        text : 'Пряма',
+        pattern : []
+    },
+    DOTS : {
+        text : 'Крапки',
+        pattern : [1, 1]
+    },
+    SIMPLE_DASH : {
+        text : 'Тире',
+        pattern : [10, 10]
+    },
+    DASH_DOT : {
+        text : 'Тире і Крапка',
+        pattern : [15, 3, 3, 3]
+    }
 }
