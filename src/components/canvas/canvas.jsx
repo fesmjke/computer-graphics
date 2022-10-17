bf2f367010d06a6ab7a9068d674be07dad781cd1
fesmjke <fatalerror.sm@gmail.com>
2022-10-18 01:10:05 +0300

    removed unuseful dependencies, updated canvas component

--- src/components/canvas/canvas.jsx
+++ src/components/canvas/canvas.jsx
@@ -1,44 +1,49 @@
-import React, { useRef, useEffect, useState, useMemo } from "react";
-import { HDrawer } from "../fractals/h-fractal/h-drawer";
-import { KochDrawer } from "../fractals/koch/koch-drawer";
-import { FRACTAL_TYPES } from "../fractals/types";
+import React, { useRef, useEffect, useState } from "react";
 import { drawLine } from "./helpers/draw";
 import "./styles.css";
 
-const Canvas = (props) => {
-    const fractal = props.fractal;
+const Canvas = ({fractal , type, styles, changeCenter}) => {
     const canvasRef = useRef(null);
-    const [type, setType] = useState(props.type);
-    const [iteration, setIteration] = useState(props.iteration);
-    const [width, setWidth] = useState(700);
-    const [height, setHeight] = useState(700);
-
-    let lines = useMemo(() => {
-        if(type === FRACTAL_TYPES.KOCH) {
-            return KochDrawer(fractal,iteration,false);
-        } else if (type === FRACTAL_TYPES.INVERSED) {
-            return KochDrawer(fractal,iteration,true);
-        } else {
-            return HDrawer(fractal,iteration)
-        }
-    },[type,fractal,iteration]);
+    const [width, setWidth] = useState(800);
+    const [height, setHeight] = useState(550);
 
     useEffect(() => {
         const canvas = canvasRef.current;
         const ctx = canvas.getContext('2d');
 
-        const connection = type === FRACTAL_TYPES.H ? false : true;
-
         ctx.reset();
 
-        for(const line of lines){
-            drawLine(ctx,line.points(),connection);
+        for(const line of fractal){
+            drawLine(ctx,line.points(),styles.connection,styles.color,styles.line);
         }
 
-        type === FRACTAL_TYPES.H ? ctx.stroke() : ctx.fill();
-      })
+        if(styles.connection){
+            ctx.fill();
+        } else {
+            ctx.strokeStyle = `rgb(${styles.color.r},${styles.color.g},${styles.color.b})`;
+            ctx.stroke();
+        }
+        ctx.save();
+    })
 
-    return <canvas ref={canvasRef} {...props} width={width} height={height}></canvas>
+    const userClick = (event) => {
+        const rect = canvasRef.current.getBoundingClientRect();
+
+        
+        const position = {
+            x: Math.round(event.clientX - rect.left),
+            y: Math.round(event.clientY - rect.top)
+        }
+
+        changeCenter(position);
+    }
+
+    return (
+        <div className="canvas">
+            <h3>{type}</h3>
+            <canvas ref={canvasRef} width={width} height={height} onClick={userClick}></canvas>
+        </div>
+    )
 }
 
 export default Canvas;
