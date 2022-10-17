1f46085e51abe1a6bbad7b848cd6db126c3efe56
fesmjke <fatalerror.sm@gmail.com>
2022-10-18 01:03:15 +0300

    changed h-tree fractal, added generator, and updated optimization

--- src/components/fractals/h-fractal/h-tree.js
+++ src/components/fractals/h-fractal/h-tree.js
@@ -1,51 +1,97 @@
 import { Line } from "../util/line";
 
 export class HTree {
-    constructor(line,limit) {
+    constructor(line) {
         this.root = line;
-        this.limit = limit;
         this.sqrt2 = Math.sqrt(2);
+        this.lines = new Map();
+        this.init = true;
     }
 
     scaled = (line) => {
         return line.length() / this.sqrt2;
     }
 
-    recursive = () => {
-        
+    * buildedLines(limit) {    
+        for(const key of this.lines.keys()){
+            if (key === limit){
+                return this.lines.get(key);
+            } else {
+                yield this.lines.get(key)
+            }
+        }
     }
 
-    build = () => {
-        let vertical = this.genVertical(this.root);
+    builded = (limit) => {
+        const generator = this.buildedLines(limit);
+        const merge = [];
+        let value;
 
-        this.root.attach();
+        do {
+            value = generator.next();
+            merge.push(value.value);
 
-        let horizontal = [];
+        } while (!value.done)
 
-        for(let i = 0; i < this.limit; i++) {
-            for (const v of vertical) {
-                horizontal.push(...this.genHorizontal(v));
-                v.attach();
-            }
-            for (const h of horizontal) {
-                vertical.push(...this.genVertical(h))
-                h.attach();
-            }   
+        const flatMerged = merge.reduce((acc, curVal) => {
+            return acc.concat(curVal)
+        }, []);
 
-            // console.log('v ',vertical.length , vertical);
-            // console.log('h ',horizontal.length , horizontal);
+        return flatMerged;
+    }
+
+    build = (limit) => {
+        if(this.init){
+            let vertical = this.genVertical(this.root);
+
+            this.root.attach();
+    
+            this.lines.set(limit,[this.root,...vertical]);
+
+            this.init = false;
+
+            return this.builded(limit);
+        }
+        
+        if(this.lines.has(limit)){
+            return this.builded(limit);
         }
 
-        return [this.root,...vertical,...horizontal];
+        if(this.lines.size >= 1){
+            this.buildNewLines(limit);
+
+            return this.builded(limit);
+        }
+    }
+
+    buildNewLines = (limit) => {
+        const filtered = this.lines.get(limit - 1).filter((l) => {return l.attached === false});
+
+        const temp = [];
+
+        for(const vline of filtered.filter((l) => l.vertical)){
+            temp.push(...this.genHorizontal(vline))
+            vline.attach();
+        }
+
+        for(const hline of temp.filter((l) => l.horizontal)){
+            temp.push(...this.genVertical(hline))
+            hline.attach();
+        }
+
+        this.lines.set(limit, temp);
     }
 
     genVertical = (line) => {
         const l = this.scaled(line);
 
-        const v = new Line(line.start.x,line.start.y - l / 2, line.start.x, line.start.y + l / 2);
-        const v1 = new Line(line.end.x, line.end.y - l / 2, line.end.x, line.end.y + l / 2);
+        const v1 = new Line(line.start.x,line.start.y - l / 2, line.start.x, line.start.y + l / 2);
+        const v2 = new Line(line.end.x, line.end.y - l / 2, line.end.x, line.end.y + l / 2);
 
-        return [v,v1];
+        v1.vertical = true;
+        v2.vertical = true;
+
+        return [v1,v2];
     }
 
     genHorizontal = (line) => {
@@ -53,8 +99,9 @@ export class HTree {
 
         const h1 = new Line(line.start.x - l / 2, line.start.y, line.start.x + l / 2, line.start.y);
         const h2 = new Line(line.start.x - l / 2, line.end.y, line.start.x + l / 2, line.end.y);
-        // const h3 = new Line();
-        // const h4 = new Line();
+
+        h1.horizontal = true;
+        h2.horizontal = true;
 
         return [h1,h2];
     }
