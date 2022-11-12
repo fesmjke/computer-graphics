5ceb25d349bf1995c4c08d384d8b049d307ec7a9
fesmjke <fatalerror.sm@gmail.com>
2022-11-12 22:17:39 +0200

    added links to about pages on entry page

--- src/components/entry/entry-page.jsx
+++ src/components/entry/entry-page.jsx
@@ -2,6 +2,7 @@ import "./styles.css";
 import FractalImage from "../../static/images/fractal.jpg";
 import Models from "../../static/images/models.png";
 import Af from "../../static/images/af-t.png";
+import { Link } from "react-router-dom";
 
 const EntryPage = () => {
     return (
@@ -15,7 +16,7 @@ const EntryPage = () => {
                         Фракта́л (від лат. fractus — подрібнений, дробовий) — у побутовому розумінні часто означають як деяку нерегулярну, самоподібну структуру.
                     </p>
                 </div>
-                <button>Детальніше</button>
+                <button><Link to={`/about/fractals`} style={{textDecoration : 'none'}}>Детальніше</Link></button>
             </div>
             <div className="card">
                 <div className="image-frame">
@@ -26,7 +27,7 @@ const EntryPage = () => {
                         Колірна модель — абстрактна модель опису представлення кольорів у вигляді кортежів чисел, зазвичай з трьох або чотирьох значень.
                     </p>
                 </div>
-                <button>Детальніше</button>
+                <button><Link to={`/about/colors`} style={{textDecoration : 'none'}}>Детальніше</Link></button>
             </div>
             <div className="card">
                 <div className="image-frame">
@@ -37,7 +38,7 @@ const EntryPage = () => {
                         Афінне перетворення — відображення площини або простору в собі.
                     </p>
                 </div>
-                <button>Детальніше</button>
+                <button><Link to={`/about/transformation`} style={{textDecoration : 'none'}}>Детальніше</Link></button>
             </div>
         </div>
     )
